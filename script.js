a0622e076204f07fc1ed96abdd7091960eea87ff
CypressWriter <cypresswriter@gmail.com>
2022-04-17 17:23:43 -0700

    completed Rock Paper Sissors

--- script.js
+++ script.js
@@ -1,36 +1,10 @@
-let score;
+let playerScore = 0;
+let computerScore = 0;
 let scoreToWin = 5;
 
-function computerPlay() {
-    let roll;
-    let result;
-    
-    roll = Math.floor(Math.random() * 3);
+let gameEnd = false;
 
-    switch (roll) {
-        case 0:
-            result = "rock";
-            break;
-        case 1:
-            result = "paper";
-            break;
-        case 2:
-            result = "sissors";
-            break;
-        default:
-            console.log("error");
-    }
-    return result;
-}
-
-
-/*
-function playerPlay() {
-    let result = window.prompt("Rock, paper, or sissors?");
-    result = result.toLowerCase();
-    return result;
-}
-*/
+const resultDiv = document.querySelector("#result");
 
 let rock = document.getElementById("rock");
 let paper = document.getElementById("paper");
@@ -41,11 +15,78 @@ paper.addEventListener("click", function () {playerPlay("paper")});
 sissors.addEventListener("click", function () {playerPlay("sissors")});
 
 function playerPlay (selection) {
-    playerSelection = selection;
-    console.log(playerSelection);
+
+    if (playerScore < 5 && computerScore < 5) {
+        playerSelection = selection;
+
+        const result = document.createElement("div");
+        result.classList.add("result");
+        result.textContent = "Player selected " + playerSelection;
+        resultDiv.appendChild(result);
+
+        const roundResultDiv = document.createElement("div");
+        roundResultDiv.classList.add("result");
+
+        roundResult = playRound(selection, computerPlay());
+
+        if (roundResult === "tie") {
+            roundResultDiv.textContent = "Tie!";
+        }
+        else if (roundResult === "win") {
+            roundResultDiv.textContent = "Player wins this round!";
+            playerScore++;
+        }
+        else if (roundResult === "lose") {
+            roundResultDiv.textContent = "Computer wins this round!";
+            computerScore++;
+        }
+
+        resultDiv.appendChild(roundResultDiv);
+    }
+    else if (playerScore === 5) {
+        result.textContent = "Player won the game!";
+        gameEnd = true;
+    }
+    else if (computerScore === 5) {
+        result.textContent = "Computer won the game!";
+        gameEnd = true;
+    }
 };
 
+function computerPlay() {
+    let roll;
+    let result;
+    
+    roll = Math.floor(Math.random() * 3);
+
+    const computerResult = document.createElement("div");
+    computerResult.classList.add("computerResult");
+
+    switch (roll) {
+        case 0:
+            result = "rock";
+            computerResult.textContent = "Computer selected " + result;
+            break;
+        case 1:
+            result = "paper";
+            computerResult.textContent = "Computer selected " + result;
+            break;
+        case 2:
+            result = "sissors";
+            computerResult.textContent = "Computer selected " + result;
+            break;
+        default:
+            console.log("error");
+    }
+
+    resultDiv.appendChild(computerResult);
+    return result;
+}
+
+
+
 function playRound(playerSelection, computerSelection) {
+
     if (playerSelection === "rock") {
         if (computerSelection === "rock") {
             return "tie";
@@ -54,6 +95,7 @@ function playRound(playerSelection, computerSelection) {
             return "lose";
         }
         if (computerSelection === "sissors") {
+            playerScore++;
             return "win";
         }
     }
